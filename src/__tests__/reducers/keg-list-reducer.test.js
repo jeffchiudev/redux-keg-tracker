e9d89d648372661d66aefb5ee8b73fbd45506919
Jeff Chiu <jwchiu82@gmail.com>
2021-02-19 09:06:46 -0800

    green pass default return state of keg list reducer

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -3,7 +3,7 @@ import kegListReducer from '../../reducers/keg-list-reducer';
 describe('kegListReducer', () => {
   
   test('should return default statei f there is no action type passed into reducer', () => {
-    expect(ticketListReducer({}, { type: null })).toEqual({});
+    expect(kegListReducer({}, { type: null })).toEqual({});
   });
 
 });
