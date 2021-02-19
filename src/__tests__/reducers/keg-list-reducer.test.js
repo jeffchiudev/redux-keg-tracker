a677423e8c2cda1a5ed1134a7cef7eb5ab61712d
Jeff Chiu <jwchiu82@gmail.com>
2021-02-19 09:23:14 -0800

    red fail for retiring keg

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -11,6 +11,24 @@ describe('kegListReducer', () => {
     quantity: '124',
     id: 1,
   };
+  const currentState = {
+    1: {
+      name: 'IPA',
+      brand: 'Breakside',
+      price: '75',
+      flavor: 'citrusy',
+      quantity: '124',
+      id: 1,
+    },
+    2 : {
+      name: 'Porter',
+      brand: 'Guiness',
+      price: '100',
+      flavor: 'oaty',
+      quantity: '124',
+      id: 2,
+    }
+  }
   
   test('should return default state if there is no action type passed into reducer', () => {
     expect(kegListReducer({}, { type: null })).toEqual({});
@@ -38,7 +56,24 @@ describe('kegListReducer', () => {
         id,
       }
     });
+  });
 
-  })
+  test('Should successfully retire a keg', () => {
+    action = {
+      type: 'DELETE_KEG',
+      id: 1
+    };
+
+    expect(kegListReducer(currentState, action)).toEqual({
+      2 : {
+        name: 'Porter',
+        brand: 'Guiness',
+        price: '100',
+        flavor: 'oaty',
+        quantity: '124',
+        id: 2,
+      }
+    });
+  });
 
 });
