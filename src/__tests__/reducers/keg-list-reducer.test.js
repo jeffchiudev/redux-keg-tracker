77845f4dd0ee52872d9a71eab881f16156a126b2
Jeff Chiu <jwchiu82@gmail.com>
2021-02-19 09:15:03 -0800

    red fail test to add keg to master keg list

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -1,9 +1,44 @@
 import kegListReducer from '../../reducers/keg-list-reducer';
 
 describe('kegListReducer', () => {
+
+  let action;
+  const kegData = {
+    name: 'Pilsner',
+    brand: 'Molson',
+    price: '20',
+    flavor: 'water',
+    quantity: '124',
+    id: 1,
+  };
   
-  test('should return default statei f there is no action type passed into reducer', () => {
+  test('should return default state if there is no action type passed into reducer', () => {
     expect(kegListReducer({}, { type: null })).toEqual({});
   });
+
+  test('should successfully add new keg data to masterKegList', () => {
+    const { name, brand, price, flavor, quantity, id } = kegData;
+    action = {
+      type: c.ADD_KEG,
+      name,
+      brand,
+      price,
+      flavor,
+      quantity,
+      id,
+    };
+
+    expect(kegListReducer({}, action)).toEqual({
+      [id] : {
+        name,
+        brand,
+        price,
+        flavor,
+        quantity,
+        id,
+      }
+    });
+    
+  })
 
 });
