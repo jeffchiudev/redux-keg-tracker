7dd76081e07c581fd5b991515f3c5f03c155d775
Jeff Chiu <jwchiu82@gmail.com>
2021-02-19 11:07:47 -0800

    green pass rootReducer uses the multiple reducers within

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -22,4 +22,26 @@ describe ("rootReducer", () => {
     expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null}))
   });
 
+  test('check that ADD_KEG works for kegListReducer and root reducer', () => {
+    const action = {
+      type: 'ADD_KEG',
+      name: 'Pilsner',
+      brand: 'Molson',
+      price: '20',
+      flavor: 'water',
+      quantity: '124',
+      id: 1,
+    }
+    store.dispatch(action);
+    expect(store.getState().masterKegList).toEqual(kegListReducer(undefined, action));
+  });
+
+  test('Check that TOGGLE_FORM action works for formVisibleReducer and root reducer', () => {
+    const action = {
+      type: 'TOGGLE_FORM'
+    }
+    store.dispatch(action);
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, action));
+  });
+
 });
