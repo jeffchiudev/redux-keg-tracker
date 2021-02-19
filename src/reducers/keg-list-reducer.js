480fcec2efceaaa6813e29fb6a7e34dc2f23a5c3
Jeff Chiu <jwchiu82@gmail.com>
2021-02-19 09:26:06 -0800

    green pass for retiring/deleting keg from list

--- src/reducers/keg-list-reducer.js
+++ src/reducers/keg-list-reducer.js
@@ -2,16 +2,22 @@ export default (state = {}, action ) => {
   const { name, brand, price, flavor, quantity, id } = action;
   switch (action.type) {
     case 'ADD_KEG':
-    return Object.assign({}, state, {
-      [id] : {
-        name,
-        brand,
-        price,
-        flavor,
-        quantity,
-        id,
-      }
-    });
+      return Object.assign({}, state, {
+        [id] : {
+          name,
+          brand,
+          price,
+          flavor,
+          quantity,
+          id,
+        }
+      });
+
+    case 'DELETE_KEG':
+      let newState = {...state};
+      delete newState[id];
+      return newState;
+
     default:
       return state;
   }
