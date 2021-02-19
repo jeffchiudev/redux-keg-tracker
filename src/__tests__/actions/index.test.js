08cf705317d761d57ae4ef6cad50c532a0010018
Jeff Chiu <jwchiu82@gmail.com>
2021-02-19 11:52:58 -0800

    red fail deleteKeg action creator

---
+++ src/__tests__/actions/index.test.js
@@ -0,0 +1,12 @@
+import * as actions from './../../actions';
+
+describe('help quue actions', () => {
+
+  it('deleteKeg should create DELETE_KEG action', () => {
+    expect(actions.deleteKeg(1)).toEqual({
+      type: 'DELETE_KEG',
+      id: 1,
+    });
+  });
+
+});
